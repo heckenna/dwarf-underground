086ba2e25619a51a65048867b2a0346ba239357b
heckenna <heckenna@rose-hulman.edu>
2018-07-03 10:37:43 -0400

    List elements appear,but they are empty

--- src/ClickBait.js
+++ src/ClickBait.js
@@ -7,7 +7,7 @@ import IndianaHobbits from './IndianaHobbits';
 
 class Baits extends Component{
     render(){
-        return((this.props.baits.name))
+        return(<li>{this.props.name}</li>)
     }
 }
 
@@ -17,16 +17,26 @@ class ClickBait extends Component {
 
         this.state = {
             baits: [
-            {name: "<OrcsInIndy />"}, 
-            {name: "<UnderMountain />"}, 
-            {name: "<MineGold />"}, 
-            {name: "<IndianaHobbits />"},
+            // {img: "https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif",
+            //     name:"Single Orcs in Indianapolis"}, 
+            // {img: "https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg", 
+            //     name:"You won't believe what's under this mountain"}, 
+            // {img: "http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg",
+            //     name: "Mine 20% more gold with One Weird Trick",}, 
+            // {img: "http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg",
+            //   name:"Surprise for Indiana Hobbits born before 1999",},
+            {name: <OrcsInIndy />},
+            {name: <UnderMountain />},
+            {name: <MineGold />},
+            {name: <IndianaHobbits />},
             ]
         }
     }
 
     render() {
         return(
+
+        
             <div className="small-12 columns other-articles">
         <h2>From around the Realm</h2>
         {/* <OrcsInIndy />
@@ -34,9 +44,10 @@ class ClickBait extends Component {
         <MineGold />
         <IndianaHobbits /> */}
         <ul>
-        {this.state.baits.forEach(element => React.createElement(element , ))}        
-        </ul>
-      </div>
+        {this.state.baits.map(currentElement => <Baits element={currentElement} />)}
+        </ul>    
+            </div>
+        
         )
     }
 }
