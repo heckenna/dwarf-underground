487323288e6afcb1503917553bab38f335bb688f
heckenna <heckenna@rose-hulman.edu>
2018-07-02 22:16:33 -0400

    Complete the Bonus Credit. Difficulty is realy with Super Bonus

---
+++ src/ClickBait.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import OrcsInIndy from './OrcsInIndy';
+import UnderMountain from './UnderMountain';
+import MineGold from './MineGold';
+import IndianaHobbits from './IndianaHobbits';
+
+class Baits extends Component {
+    render(){
+        return(jQuery.parseHTML(this.props.baits.name))
+    }
+}
+
+class ClickBait extends Component {
+    // constructor() {
+    //     super()
+
+    //     this.state = {
+    //         baits: [
+    //         {name: "<OrcsInIndy />"}, 
+    //         {name: "<UnderMountain />"}, 
+    //         {name: "<MineGold />"}, 
+    //         {name: "<IndianaHobbits />"},
+    //         ]
+    //     }
+    // }
+
+    render() {
+        return(
+            <div className="small-12 columns other-articles">
+        <h2>From around the Realm</h2>
+        <OrcsInIndy />
+        <UnderMountain />
+        <MineGold />
+        <IndianaHobbits />
+        {/* <ul>
+        {this.state.baits.map((currentBait, i) => <Baits name={currentBait} key={i}/>)}        
+        </ul> */}
+      </div>
+        )
+    }
+}
+
+export default ClickBait;
