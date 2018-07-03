f8961b3a140489b3974fa13a94716da87ea7b378
heckenna <heckenna@rose-hulman.edu>
2018-07-03 09:52:55 -0400

    Code compile again, but clickbait doesn't show

--- src/ClickBait.js
+++ src/ClickBait.js
@@ -4,37 +4,38 @@ import UnderMountain from './UnderMountain';
 import MineGold from './MineGold';
 import IndianaHobbits from './IndianaHobbits';
 
-class Baits extends Component {
+
+class Baits extends Component{
     render(){
-        return(this.props.baits.name)
+        return((this.props.baits.name))
     }
 }
 
 class ClickBait extends Component {
-    // constructor() {
-    //     super()
+    constructor() {
+        super()
 
-    //     this.state = {
-    //         baits: [
-    //         {name: "<OrcsInIndy />"}, 
-    //         {name: "<UnderMountain />"}, 
-    //         {name: "<MineGold />"}, 
-    //         {name: "<IndianaHobbits />"},
-    //         ]
-    //     }
-    // }
+        this.state = {
+            baits: [
+            {name: "<OrcsInIndy />"}, 
+            {name: "<UnderMountain />"}, 
+            {name: "<MineGold />"}, 
+            {name: "<IndianaHobbits />"},
+            ]
+        }
+    }
 
     render() {
         return(
             <div className="small-12 columns other-articles">
         <h2>From around the Realm</h2>
-        <OrcsInIndy />
+        {/* <OrcsInIndy />
         <UnderMountain />
         <MineGold />
-        <IndianaHobbits />
-        {/* <ul>
-        {this.state.baits.map((currentBait, i) => <Baits name={currentBait} key={i}/>)}        
-        </ul> */}
+        <IndianaHobbits /> */}
+        <ul>
+        {this.state.baits.forEach(element => React.createElement(element , ))}        
+        </ul>
       </div>
         )
     }
